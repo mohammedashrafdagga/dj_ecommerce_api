8ab0c67f83532ef2c404cff023e7435b4054cbef
Mohammed Dagga <47271587+mohammedashrafdagga@users.noreply.github.com>
2024-06-08 18:28:15 +0300

    setup project with install library and sprate settings files

---
+++ ecommerce/settings/local.py
@@ -0,0 +1,16 @@
+from decouple import config
+
+from .base import *
+
+DEBUG = config("DEBUG", default=True, cast=bool)
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DATABASE_NAME_LOCAL"),
+        "USER": config("DATABASE_USER_LOCAL"),
+        "PASSWORD": config("DATABASE_PASSWORD_LOCAL"),
+        "HOST": config("DATABASE_HOST_LOCAL", default="localhost"),
+        "PORT": config("DATABASE_PORT_LOCAL", default="5432", cast=int),
+    }
+}
