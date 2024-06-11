0b53ba0a1373ebb749ba77e64292e4ba53f1b1b2
Mohammed Dagga <47271587+mohammedashrafdagga@users.noreply.github.com>
2024-06-11 11:46:30 +0300

    create product and product image model

--- ecommerce/apps/products/admin.py
+++ ecommerce/apps/products/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Brand, Category
+from .models import Brand, Category, Product, ProductImage
 
 
 # register Category
@@ -19,3 +19,24 @@ class BrandAdmin(admin.ModelAdmin):
     search_fields = ("name",)
     readonly_fields = ("create_at", "update_at")
     filter_horizontal = ("categories",)  # To use a better UI for ManyToManyField
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 3
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug", "price", "stock", "category", "brand")
+    prepopulated_fields = {"slug": ("name",)}
+    search_fields = ("name",)
+    list_filter = ("category", "brand", "create_at")
+    readonly_fields = ("create_at", "update_at")
+    inlines = [ProductImageInline]
+
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ("product", "image", "create_at")
+    readonly_fields = ("create_at",)
--- ecommerce/apps/products/models.py
+++ ecommerce/apps/products/models.py
@@ -26,3 +26,34 @@ class Brand(models.Model):
 
     def __str__(self):
         return f"{self.name}"
+
+
+# Product Model
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.CharField(max_length=255, unique=True, blank=True)
+    content = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    main_image = models.ImageField(upload_to="products/")
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="products"
+    )
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name="products")
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+#  product Images Model
+class ProductImage(models.Model):
+    product = models.ForeignKey(
+        Product, related_name="images", on_delete=models.CASCADE
+    )
+    image = models.ImageField(upload_to="product_images/")
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Image for {self.product.name}"
